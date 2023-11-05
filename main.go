c06a8089962da9a56f9a440f3349bbf6807f8e9c
rovezuka <rostislav.04@list.ru>
2023-11-05 16:05:40 +0300

    Implemented POST method and added comments

--- main.go
+++ main.go
@@ -2,22 +2,31 @@ package main
 
 import (
 	"fmt"
-	"github.com/rovezuka/go-api-cats/api"
 	"log"
+
+	"github.com/rovezuka/go-api-cats/api"
 )
 
+const API_KEY = "YOUR_API_KEY" // Константа для хранения ключа API
+
 func main() {
+	// Создание нового клиента API
 	apiClient, err := api.NewClient()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	catImages, err := apiClient.GetAsset("10", "live_c1vDY98eMovuABHvsqTjkgnKRs1qm5vnDatRDMKE7crDh94FMmWopnxgq9Jd8wvd")
+	// Выполнение GET-запроса для получения изображений
+	catImages, err := apiClient.GetAsset("10", API_KEY)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Вывод информации об изображениях
 	for _, catImage := range catImages {
 		fmt.Println(catImage.Info())
 	}
+
+	// Выполнение POST-запроса для отправки файла
+	apiClient.PostAsset("FILE_PATH", "POST_URL", API_KEY)
 }
